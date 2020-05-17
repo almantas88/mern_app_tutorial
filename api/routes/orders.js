68dc67d4961e445c5a8e5b08bb496e45c630f2a0
almantas88 <almantas88@gmail.com>
2020-05-17 21:50:53 +0300

    Initial commit

---
+++ api/routes/orders.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Handling GET request to /orders"
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Handling POST request to /orders"
+    });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: "order deleted"
+    });
+});
+
+module.exports = router;
