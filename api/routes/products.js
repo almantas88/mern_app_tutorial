2bc0d3c8482dc12b52dac7697308363c20189c31
almantas88 <almantas88@gmail.com>
2020-05-27 17:16:30 +0300

    Added data validations and added better responses

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,17 +6,31 @@ const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
   Product.find()
+    .select("name price _id")
     .exec()
     .then((docs) => {
-      console.log(docs);
-    //   if(docs.length >= 0){
-        res.status(200).json(docs);
-    //   }
-    //   else{
-    //     res.status(404).json({
-    //         message: "No entries found"
-    //     });
-    //   }
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: 'GET',
+              url: "http://localhost:3000/products/" + doc._id
+            }
+          }
+        })
+      }
+      //   if(docs.length >= 0){
+      res.status(200).json(response);
+      //   }
+      //   else{
+      //     res.status(404).json({
+      //         message: "No entries found"
+      //     });
+      //   }
     })
     .catch((err) => {
       console.log(err);
@@ -35,8 +49,16 @@ router.post("/", (req, res, next) => {
     .then((result) => {
       console.log(result);
       res.status(201).json({
-        message: "Handling POST request to /products",
-        createdProduct: result,
+        message: "Created a product successfully",
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: 'GET',
+            url: "http://localhost:3000/products/" + result._id
+          }
+        }
       });
     })
     .catch((err) => {
@@ -45,16 +67,25 @@ router.post("/", (req, res, next) => {
         error: err,
       });
     });
+
 });
 
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select("name price _id")
     .exec()
     .then((doc) => {
       console.log(doc);
       if (doc) {
-        res.status(200).json({ doc });
+        res.status(200).json({ 
+          product:doc,
+          request:{
+            type: "GET",
+            description: "Get all products",
+            url: "http://localhost:3000/products"
+          }
+         });
       } else {
         res
           .status(404)
@@ -68,39 +99,52 @@ router.get("/:productId", (req, res, next) => {
 });
 
 router.patch("/:productId", (req, res, next) => {
-    const id = req.params.productId;
-    const updateOps = {};
-        for (const ops of req.body){
-            updateOps[ops.propName] = ops.value;
-        }
-    console.log(req.body);
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  console.log(req.body);
   Product.update({ _id: id }, { $set: updateOps })
-  .exec()
-  .then(result => {
-      console.log(result);
-      res.status(200).json(result);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({
-        error: err
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: 'Product updated',
+        request:{
+          type: "GET",
+          url: "http://localhost:3000/products/" + id
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
     });
-  })
 });
 
 router.delete("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.remove({ _id: id })
-  .exec()
-  .then(result => {
-      res.status(200).json(result);
-  })
-  .catch(err =>{
-    console.log(err);
-    res.status(500).json({
-        error: err
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "Product deleted",
+        request: {
+          description: "Creating a new product",
+          type: "POST",
+          url: "http://localhost:3000/products/",
+          body: {name: 'String', price: 'Number'}
+        }
+      });
     })
-  });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
