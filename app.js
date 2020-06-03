da4785fec75dd59ff51583e6389c7b7ce9ac80c5
almantas88 <almantas88@gmail.com>
2020-06-03 13:55:36 +0300

    Sign up added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const uri = 'mongodb+srv://almantas88:almantas88@node-rest-shop-kt3ov.mongodb.ne
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect(uri, {  useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
     );
@@ -36,6 +37,7 @@ app.use((req,res,next)=>{
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
